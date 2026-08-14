e077bd1881719724c4a5d9d7a1e324ec73c1e80c
Daniel <dfquack08@gmail.com>
2026-08-14 15:56:47 -0600

    implement: View Binding support

--- app/src/main/java/sv/edu/udb/dsm_desafio01/MainActivity.kt
+++ app/src/main/java/sv/edu/udb/dsm_desafio01/MainActivity.kt
@@ -1,16 +1,20 @@
 package sv.edu.udb.dsm_desafio01
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import sv.edu.udb.dsm_desafio01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
